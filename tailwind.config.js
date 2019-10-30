b083bad2f0eb3dd7b60e7f048c8687e792e5f5b5
Mehul Patel <mehul8@gmail.com>
2019-10-29 20:26:20 -0400

    add hover/active/focus styles to c2a btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   theme: {
     extend: {}
   },
-  variants: {},
+  variants: {
+    // order of array matters. higner index has priority
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+  },
   plugins: []
 }
